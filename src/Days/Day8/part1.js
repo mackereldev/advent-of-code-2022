88c744c3e07a4925bfa952fba1d1ebc8fb0ef1ae
Saba Shaw <sabashaw.home@gmail.com>
2022-12-08 17:12:31 +1100

    Add Day 8

---
+++ src/Days/Day8/part1.js
@@ -0,0 +1,68 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { readFileSync } from "fs";
+
+const data = readFileSync(`${dirname(fileURLToPath(import.meta.url))}/input.txt`);
+const lines = data.toString().split("\r\n")
+
+let trees = new Array(lines.length);
+for (let i = 0; i < lines.length; i++) {
+    trees[i] = new Array(lines[i].length);
+    trees[i] = lines[i].split("").map(item => parseInt(item));
+}
+
+let visibleCount = 0;
+for (let i = 0; i < trees.length; i++) {
+    for (let j = 0; j < trees[i].length; j++) {
+        if (i != 0 && i != lines.length-1 && j != 0 && j != trees[i].length-1) {
+            if (isVisible(trees, j, i)) {
+                visibleCount++;
+            }
+        } else {
+            visibleCount++;
+        }
+    }
+}
+
+function isVisible(trees, x, y) {
+    const tree = trees[y][x];
+    let visibleScore = 4;
+
+    // Vertical
+    for (let i = 0; i < trees.length; i++) {
+        const targetTree = trees[i][x];
+        if (i != y) {
+            if (targetTree >= tree) {
+                visibleScore--;
+                if (i < y) {
+                    i = y;
+                    continue;
+                }
+                if (i > y) {
+                    break;
+                }
+            }
+        }
+    }
+    
+    // Horizontal
+    for (let i = 0; i < trees[y].length; i++) {
+        const targetTree = trees[y][i];
+        if (i != x) {
+            if (targetTree >= tree) {
+                visibleScore--;
+                if (i < x) {
+                    i = x;
+                    continue;
+                }
+                if (i > x) {
+                    break;
+                }
+            }
+        }
+    }
+
+    return visibleScore != 0;
+}
+
+console.log(visibleCount);
---
+++ src/Days/Day8/part2.js
@@ -0,0 +1,69 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { readFileSync } from "fs";
+
+const data = readFileSync(`${dirname(fileURLToPath(import.meta.url))}/input.txt`);
+const lines = data.toString().split("\r\n")
+
+let trees = new Array(lines.length);
+for (let i = 0; i < lines.length; i++) {
+    trees[i] = new Array(lines[i].length);
+    trees[i] = lines[i].split("").map(item => parseInt(item));
+}
+
+let highestScenicScore = 0;
+for (let i = 0; i < trees.length; i++) {
+    for (let j = 0; j < trees[i].length; j++) {
+        if (i != 0 && i != lines.length-1 && j != 0 && j != trees[i].length-1) {
+            highestScenicScore = Math.max(highestScenicScore, getScenicScore(trees, j, i));
+        }
+    }
+}
+
+function getScenicScore(trees, x, y) {
+    const tree = trees[y][x];
+
+    // Up
+    let upScore = 0;
+    for (let i = y-1; i >= 0; i--) {
+        const targetTree = trees[i][x];
+        upScore++;
+        if (targetTree >= tree) {
+            break;
+        }
+    }
+    
+    // Down
+    let downScore = 0;
+    for (let i = y+1; i < trees.length; i++) {
+        const targetTree = trees[i][x];
+        downScore++;
+        if (targetTree >= tree) {
+            break;
+        }
+    }
+    
+    // Left
+    let leftScore = 0;
+    for (let i = x-1; i >= 0; i--) {
+        const targetTree = trees[y][i];
+        leftScore++;
+        if (targetTree >= tree) {
+            break;
+        }
+    }
+    
+    // Right
+    let rightScore = 0;
+    for (let i = x+1; i < trees[y].length; i++) {
+        const targetTree = trees[y][i];
+        rightScore++;
+        if (targetTree >= tree) {
+            break;
+        }
+    }
+
+    return upScore*downScore*leftScore*rightScore;
+}
+
+console.log(highestScenicScore);
