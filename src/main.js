9d036510e3b5bed2236ce9de7195d1acaf0d6834
mackeraldev <sabashaw.home@gmail.com>
2023-02-21 13:34:48 +1100

    Polish prompt system

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ let days = [];
 const files = readdirSync("src/Days");
 for (let i = 0; i < files.length; i++) {
     const file = files[i];
-    
+
     let day = new Day();
     day.name = file;
 
@@ -32,46 +32,43 @@ for (let i = 0; i < files.length; i++) {
     days.push(day);
 }
 
-inquirer.prompt([
-    {
-        type: "list",
-        message: "Pick a day:",
-        name: "day",
-        choices: days.map(item => item.name).sort((a, b) => a === b ? 0 : a > b ? -1 : 1)
-    }
-])
-.then(dayAnswer => {
-    let dayName = dayAnswer.day;
-    let day = days.find(item => item.name == dayAnswer.day);
+inquirer.prompt([{
+    type: "list",
+    message: "Pick a day:",
+    name: "day",
+    choices: days.map(item => item.name).sort((a, b) => parseInt(a.match(/(\d+)/)) - parseInt(b.match(/(\d+)/))),
+    loop: false
+}])
+    .then(dayAnswer => {
+        let dayName = dayAnswer.day;
+        let day = days.find(item => item.name == dayAnswer.day);
 
-    if (day.parts.length == 0) {
-        console.error(chalk.red(`${dayName} does not contain any part files`));
-        return;
-    }
-    
-    if (!day.hasInput) {
-        console.error(chalk.red(`${dayName} does not contain an input file`));
-        return;
-    }
-    
-    inquirer.prompt([
-        {
+        if (day.parts.length == 0) {
+            console.error(chalk.red(`${dayName} does not contain any part files`));
+            return;
+        }
+
+        if (!day.hasInput) {
+            console.error(chalk.red(`${dayName} does not contain an input file`));
+            return;
+        }
+
+        inquirer.prompt([{
             type: "list",
             message: "Pick a part:",
             name: "part",
             choices: day.parts.sort((a, b) => a === b ? 0 : a < b ? -1 : 1)
-        }
-    ])
-    .then(partAnswer => {
-        let partName = partAnswer.part;
-        exec(`node ./src/Days/${dayName}/${partName}`, (error, stdout, stderr) => {
-            if (error == null) {
-                let output = stdout.replace(/([\r\n])+/g, "");
-                console.log(`${dayName} ${partName}: ${output}`);
-            } else {
-                console.error(chalk.red(`An error occured: ${error.name} (${error.code})`));
-            }
-            return (error != null) ? error.code : 0;
-        })
+        }])
+            .then(partAnswer => {
+                let partName = partAnswer.part;
+                exec(`node ./src/Days/${dayName}/${partName}`, (error, stdout, stderr) => {
+                    if (error == null) {
+                        let output = stdout.replace(/([\r\n])+/g, "");
+                        console.log(`${dayName}/${partName}: ${output}`);
+                    } else {
+                        console.error(chalk.red(`An error occured: ${error.name} (${error.code})`));
+                    }
+                    return (error != null) ? error.code : 0;
+                })
+            })
     })
-})
