de1c21696b6f031e11e4bff2c9a02d0916d5f29d
mackeraldev <sabashaw.home@gmail.com>
2023-02-24 21:47:42 +1100

    Refine Day 15 part 1

--- src/Days/Day15/part1.js
+++ src/Days/Day15/part1.js
@@ -15,28 +15,50 @@ for (let i = 0; i < lines.length; i++) {
 }
 
 const rowCheck = 2000000;
-let checkedColumns = new Set();
 
+let ignore = new Set();
+let initialRanges = [];
 for (let i = 0; i < pairs.length; i++) {
     const pair = pairs[i];
     const radius = Math.abs(pair[1][0] - pair[0][0]) + Math.abs(pair[1][1] - pair[0][1]);
-    const yDist = Math.abs(rowCheck - pair[0][1]);
-    const checkMin = pair[0][0] - (radius - yDist);
-    const checkMax = pair[0][0] + (radius - yDist);
+    const yDist = Math.abs(pair[0][1] - rowCheck);
+    const coverageRadius = radius - yDist;
+    const coverMin = pair[0][0] - coverageRadius;
+    const coverMax = pair[0][0] + coverageRadius;
     
-    for (let j = checkMin; j <= checkMax; j++) {
-        checkedColumns.add(j);
+    if (coverageRadius >= 0) {
+        initialRanges.push([coverMin, coverMax]);
+    }
+
+    if (pair[0][1] == rowCheck) {
+        ignore.add(pair[0][0]);
+    }
+    if (pair[1][1] == rowCheck) {
+        ignore.add(pair[1][0]);
     }
 }
 
-// Prune known beacons
-for (let i = 0; i < pairs.length; i++) {
-    const pair = pairs[i];
-    if (pair[1][1] == rowCheck) {
-        if (checkedColumns.has(pair[1][0])) {
-            checkedColumns.delete(pair[1][0]);
+initialRanges.sort((a, b) => a[0] - b[0]);
+
+let reducedRanges = [[...initialRanges[0]]];
+for (let i = 1; i < initialRanges.length; i++) {
+    const range = initialRanges[i];
+    for (let j = 0; j < reducedRanges.length; j++) {
+        const reducedRange = reducedRanges[j];
+        if ((range[0] >= reducedRange[0] && range[0] <= reducedRange[1]) || (range[1] >= reducedRange[0] && range[1] <= reducedRange[1])) {
+            reducedRange[0] = Math.min(range[0], reducedRange[0]);
+            reducedRange[1] = Math.max(range[1], reducedRange[1]);
+        } else if (j == reducedRanges.length - 1) {
+            reducedRanges.push(range);
         }
     }
 }
 
-console.log(checkedColumns.size);
+let totalCoverage = 0;
+for (let i = 0; i < reducedRanges.length; i++) {
+    const range = reducedRanges[i];
+    totalCoverage += range[1] - range[0] + 1;
+    totalCoverage -= [...ignore].filter((item) => item >= range[0] && item <= range[1]).length;
+}
+
+console.log(totalCoverage);
